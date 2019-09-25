94ed21701a3275326c14ee5d8a993a60612a751f
Michael Kania <dynamike@truss.works>
2019-09-25 15:52:07 -0700

    remove old circleci config file and include issue around provider handling.

--- providers.tf
+++ providers.tf
@@ -1,6 +1,7 @@
 # Terraform 0.12 requires providers that aren't implicitly passed into the
-# module. No "region" or "version" is needed as those will be defined in
-# where the module is being instantiated.
+# module. Adding "region" will prevent us from being able to remove this
+# module.
+# See https://github.com/hashicorp/terraform/issues/22907 for details.
 provider "aws" {
   alias = "us-east-1"
 }
