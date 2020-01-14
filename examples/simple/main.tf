351ff4d72d6d07441cf7f3c695de8c8b5e57e7d3
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2020-01-14 10:03:36 -0600

    Tests pass

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,3 +1,14 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# BASIC TERRAFORM CONTENT EXAMPLE
-# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_route53_zone" "my_zone" {
+  name = var.zone_id
+
+}
+module "r53_query_logging" {
+  source = "../.."
+
+  providers = {
+    aws.us-east-1 = aws.us-east-1
+  }
+
+  logs_cloudwatch_retention = 30
+  zone_id                   = aws_route53_zone.my_zone.zone_id
+}
