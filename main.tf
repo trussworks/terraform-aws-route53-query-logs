db50168e3486490fcfb34ddcdf293de75b0aa339
Chris Gilmer <chris@truss.works>
2020-01-14 12:11:51 -0800

    Update terraform 0.12 syntax

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ data "aws_route53_zone" "main" {
 }
 
 resource "aws_cloudwatch_log_group" "main" {
-  provider = "aws.us-east-1"
+  provider = aws.us-east-1
 
   name              = "/aws/route53/${data.aws_route53_zone.main.name}"
   retention_in_days = var.logs_cloudwatch_retention
 }
 
 resource "aws_cloudwatch_log_resource_policy" "main" {
-  provider        = "aws.us-east-1"
+  provider        = aws.us-east-1
   policy_document = data.aws_iam_policy_document.main.json
   policy_name     = "route53-query-logging-policy-${var.zone_id}"
 }
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "main" {
 }
 
 resource "aws_route53_query_log" "main" {
-  depends_on = ["aws_cloudwatch_log_resource_policy.main"]
+  depends_on = [aws_cloudwatch_log_resource_policy.main]
 
   cloudwatch_log_group_arn = aws_cloudwatch_log_group.main.arn
   zone_id                  = var.zone_id
