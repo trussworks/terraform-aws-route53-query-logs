6a79d6fd50f4a785e23298addde6ad7729040b0c
Michael Kania <dynamike@truss.works>
2021-08-04 15:54:21 -0700

    fix providers to work in tf15

--- versions.tf
+++ versions.tf
@@ -1,7 +1,10 @@
 terraform {
   required_version = ">= 0.13"
-
   required_providers {
-    aws = ">= 3.0"
+    aws = {
+      source                = "hashicorp/aws"
+      version               = ">= 3.0"
+      configuration_aliases = [aws.us-east-1]
+    }
   }
 }
